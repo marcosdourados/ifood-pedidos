14065fe363bfc886142f5f7e87756c8e91362071
Marcos Roberto Vicente dos Santos <marcosrobertovicentedossantos@Mac-mini-de-Marcos.local>
2025-02-15 10:38:25 -0300

    Primeiro commit: Inicialização do projeto iFood Pedidos

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sistema de Pedidos iFood</h1>
+        <p>Bem-vindo ao nosso sistema de gerenciamento de pedidos</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
